73abb624dc78284178e122449f947370eb7dc69e
Maciej-Szeremeta <maciej.szeremeta@o2.pl>
2022-09-02 00:39:12 +0200

    Init DB Connection

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@ import chalk from 'chalk';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import express, {
-  /* urlencoded, * / /* static as eStatic, */ json, Router,
-} from 'express';
+import express, { json, Router, } from 'express';
 import rateLimit from 'express-rate-limit';
 
 // * Import plików konfiguracyjnych
@@ -21,14 +19,7 @@ import './utils/db';
 import { handleError, } from './utils/error';
 
 // Import ścieżki API
-import { homeRouter, } from './routers/home';
-import { cookieRouter, } from './routers/cookie';
-
-import { passportRouter, } from './routers/passport/auth';
-
-import { childRouter, } from './routers/child';
-import { giftRouter, } from './routers/gift';
-import { adRouter, } from './routers/map';
+// import { homeRouter, } from './routers/home';
 
 // * Aktywacja Express
 const app = express ();
@@ -58,31 +49,12 @@ app.use (morgan ('dev'));
 // * Dane przychodzące  do express z FE fetch, axios
 app.use (json ());
 
-// Ścieżki API
-app.use (
-  '/', homeRouter
-);
-app.use (
-  '/cookie', cookieRouter
-);
-
-// * Ścieżka Autentykacja Passport
-app.use (
-  '/auth', passportRouter
-);
-
-app.use (
-  '/child', childRouter
-);
-app.use (
-  '/gift', giftRouter
-);
-
 // * Dodawanie prefix do ścieżek i zmiana ścieżek z app na router
 const router = Router ();
-router.use (
-  '/ad', adRouter
-);
+
+// router.use (
+//   '/ad', adRouter
+// );
 app.use (
   '/api', router
 );
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,14 +1,14 @@
 // Połączenie mysql2/promise lokalnie
 import chalk from 'chalk';
 import { createPool, } from 'mysql2/promise';
-import { config as DB, } from '../config/config';
+import { DB, } from '../config/config';
 
 const config = {
-  host             : DB.localhost.MYSQL_HOST,
-  user             : DB.localhost.MYSQL_USER,
-  password         : DB.localhost.MYSQL_PASSWORD,
-  port             : DB.localhost.MYSQL_PORT,
-  database         : DB.localhost.MYSQL_DATABASE,
+  host             : DB.MYSQL_HOST,
+  user             : DB.MYSQL_USER,
+  password         : DB.MYSQL_PASSWORD,
+  port             : DB.MYSQL_PORT,
+  database         : DB.MYSQL_DATABASE,
   namedPlaceholders: true,
   decimalNumbers   : true,
 };
